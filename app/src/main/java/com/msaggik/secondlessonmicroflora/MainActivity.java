6879fa8ab32327376805c262d603674cc32f0e63
saLamander12m2 <michaelvl2009.123@gmail.com>
2024-02-20 13:47:41 +0300

    Initial commit

---
+++ app/src/main/java/com/msaggik/secondlessonmicroflora/MainActivity.java
@@ -0,0 +1,79 @@
+package com.msaggik.secondlessonmicroflora;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    // поля
+    private TextView output;
+    private Button button;
+    private int count = 0;
+    private int preCount = 0;
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Старт активности", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Активность доступна для взаимодействия", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Активность не доступна для взаимодействия", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Активность остановлена и пропала с экрана", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Активность уничтожена", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Активность создана", Toast.LENGTH_SHORT).show();
+
+
+        output = findViewById(R.id.output);
+        button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(view -> output.setText("" + countFibonacci()));
+
+    }
+
+    private int countFibonacci() {
+        if (count == 0 && preCount == 0) {
+            count = 1; preCount = 0;
+        } else {
+            int pre = count;
+            count = count + preCount;
+            preCount = pre;
+        }
+        return count;
+    }
+}
